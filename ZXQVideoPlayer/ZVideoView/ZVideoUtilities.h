24b10e8e226dd2ca8f403bcbc86d3a138b577e73
xqzh <xqzh@ideabinder.com>
2015-12-22 18:10:29 +0800

    粗略功能

---
+++ ZXQVideoPlayer/ZVideoView/ZVideoUtilities.h
@@ -0,0 +1,13 @@
+//
+//  ZVideoUtilities.h
+//  ZXQVideoPlayer
+//
+//  Created by Xiaoqiang Zhang on 15/12/22.
+//  Copyright © 2015年 Xiaoqiang Zhang. All rights reserved.
+//
+
+#ifndef ZVideoUtilities_h
+#define ZVideoUtilities_h
+
+
+#endif /* ZVideoUtilities_h */
