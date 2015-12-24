d33612d06928469829df07b1600cb9c2b351b139
xqzh <xqzh@ideabinder.com>
2015-12-24 12:06:55 +0800

    add swipeHandler

---
+++ ZXQVideoPlayer/ZVideoView/ZVideoSwipeHandler.h
@@ -0,0 +1,13 @@
+//
+//  ZVideoSwipeHandler.h
+//  ZXQVideoPlayer
+//
+//  Created by Xiaoqiang Zhang on 15/12/24.
+//  Copyright © 2015年 Xiaoqiang Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZVideoSwipeHandler : NSObject
+
+@end
---
+++ ZXQVideoPlayer/ZVideoView/ZVideoSwipeHandler.m
@@ -0,0 +1,13 @@
+//
+//  ZVideoSwipeHandler.m
+//  ZXQVideoPlayer
+//
+//  Created by Xiaoqiang Zhang on 15/12/24.
+//  Copyright © 2015年 Xiaoqiang Zhang. All rights reserved.
+//
+
+#import "ZVideoSwipeHandler.h"
+
+@implementation ZVideoSwipeHandler
+
+@end
